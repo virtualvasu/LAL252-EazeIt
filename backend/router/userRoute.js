6724e4d07e0fd7ef5907839dd5b7969a83b5edfb
virtualvasu <virtualvasu624@gmail.com>
2024-11-16 23:22:40 +0530

    remove backend

--- backend/router/userRoute.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const {func} = require('../controllers/controllers');
-
-const userRoute = express.Router();
-
-userRoute.route('/').get(func);
-
-module.exports = userRoute;
